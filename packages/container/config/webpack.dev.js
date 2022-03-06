1d84d02fe025a0f04b1756f0e8422f17857d3195
Santiago Calvo <santic@vitoria.local>
2022-03-06 19:27:54 +0100

    Added dashboard

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -10,16 +10,15 @@ const devConfig = {
     },
     devServer: {
         port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+        historyApiFallback: true
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js'
+                auth: 'auth@http://localhost:8082/remoteEntry.js',
+                dashboard: 'dashboard@http://localhost:8083/remoteEntry.js'
             },
             shared: packageJson.dependencies
         }),
